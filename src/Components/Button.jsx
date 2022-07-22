d5687823a6cfeea6fc59e102710e6337cfc245e9
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-07-22 18:26:27 -0300

    Feat: elementos css posiçoes

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Style/Button.css';
 
 function Button({ children, disable, onClick }) {
   return (
     <button
       type="button"
-      className="btn btn-primary mt-3"
+      className="btn btn-primary mt-3 width-max"
       disabled={disable}
       onClick={onClick}
     >
